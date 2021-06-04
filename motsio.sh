26f94759d7545db9468612f307baa77773bee284
shimla2122 <79090873+shimla2122@users.noreply.github.com>
2021-06-04 21:16:01 +0530

    Update and rename psloe.sh to motsio.sh

--- motsio.sh
+++ motsio.sh
@@ -21,7 +21,7 @@
 #
 # Wallets
 #
-source ./terahz/wallets.sh
+source ./stoild/wallets.sh
 
 # suprnova.cc pool login
 readonly MINER_WEBLOGIN=bitsbetrippin
@@ -39,7 +39,7 @@ export GPU_SINGLE_ALLOC_PERCENT=100
 
 readonly MAX_MENU_OPTIONS=37
 
-readonly ETHMINER_NVIDIA_OPTIMIZED=./terahz/Miners/ethminer/v0.11.0_Nvidia_Optimized/Linux/ethminer
+readonly ETHMINER_NVIDIA_OPTIMIZED=./stoild/Miners/ethminer/v0.11.0_Nvidia_Optimized/Linux/ethminer
 readonly CLAYMORE_DUAL_ETHEREUM=./Miners/Claymore/Dual_Ethereum/v9.7/Linux/ethdcrminer64
 readonly CLAYMORE_CRYPTONOTE=./Miners/Claymore/CryptoNote_GPU/v9.1/Linux/miner
 readonly CLAYMORE_ZCASH_AMD_GPU=./Miners/Claymore/Claymore_ZCash_AMD_GPU/v12.5/Linux/zecminer64
